8c86c00a3593805ba5c795d82bf78ac2aeed8ccd
Erick Wendel <erick.workspace@gmail.com>
2019-01-03 00:21:42 -0200

    folder order
    Signed-off-by: Erick Wendel <erick.workspace@gmail.com>

---
+++ chapter01/1.eslint-example/index.js
@@ -0,0 +1,7 @@
+class ExAmple {
+    bool_FUNCTION_1(argum3nt) {
+        return 'Awesome code pattern! ' + argum3nt
+    }
+}
+var _result = new ExAmple()
+console.log(_result.bool_FUNCTION_1("My argument!"))
